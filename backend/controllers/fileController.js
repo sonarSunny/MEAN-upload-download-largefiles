8d6b69a8a504638478c8c78a33381d279e77ce92
Your Name <you@example.com>
2020-04-11 23:47:10 +0530

    minor-bugs and fixes in backend

--- backend/controllers/fileController.js
+++ backend/controllers/fileController.js
@@ -39,14 +39,14 @@ module.exports.processFile =(req,res)=>{
 
 }
 module.exports.getFile =async(req,res)=>{
- const apidata =await filedata.find({},{_id:0,ts:1,val:1}).limit(50);
+ const apidata =await filedata.find({},{_id:0,ts:1,val:1}).limit(500);
 console.log(apidata.length)
  parseAsync(JSON.parse(JSON.stringify(apidata)))
   .then(csv =>{
     // console.log(csv)
     res.set('Content-Type', 'text/csv');
     res.setHeader('Content-Disposition', 'attachment; filename=data.csv');
-    return res.send(true)
+    return res.send(csv)
   })
   .catch(err => {
     // console.error(err)
